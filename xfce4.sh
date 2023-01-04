f464fa65433b7cdd7709b5d75ac1e2c5bda5ee4f
sanjrani786 <121862847+sanjrani786@users.noreply.github.com>
2023-01-04 14:11:04 +0500

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -10,8 +10,12 @@ sudo apt-get update > /dev/null 2>&1
 echo "===================================="
 echo "Install Firefox"
 echo "===================================="
-sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
-sudo apt install google-chrome-stable
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser -y > /dev/null 2>&1
+
 sudo apt install chromium-browser
 echo "===================================="
 echo "Install RDP"
