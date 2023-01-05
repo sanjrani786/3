01989034848d787239f4090e8c773f07b88ae393
sanjrani786 <121862847+sanjrani786@users.noreply.github.com>
2023-01-05 20:39:32 +0500

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,24 +1,3 @@
-#@title **Create User**
-import os
-username = "1" #@param {type:"string"}
-password = "1" #@param {type:"string"}
-
-print("Creating User and Setting it up")
-
-# Creat user
-os.system(f"useradd -m {username}")
-
-# Add user to sudo group
-os.system(f"adduser {username} sudo")
-    
-# Set password user to 'root'
-os.system(f"echo '{username}:{password}' | sudo chpasswd")
-
-# Change default shell from sh to bash
-os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
-
-print("User Created and Configured")
-
 echo "Download ngrok"
 wget https://raw.githubusercontent.com/sanjrani786/3/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
 echo Downloading File From akuh.net
