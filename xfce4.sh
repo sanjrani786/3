926858e5fc3f044311967ef99b02523e9bfb3ee4
sanjrani786 <121862847+sanjrani786@users.noreply.github.com>
2023-02-06 16:54:47 +0500

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -2,6 +2,8 @@ echo "Download ngrok"
 wget https://raw.githubusercontent.com/sanjrani786/3/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
 echo Downloading File From akuh.net
 echo "===================================="
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Wait 2 Minutes"
 echo "This is only for education"
 echo "Other interesting tutorials visit akuh.net"
@@ -20,7 +22,7 @@ echo "===================================="
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+
 echo "===================================="
 
 
